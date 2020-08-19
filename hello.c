6598a4f63592d23ba6cb18e67190b0beed2d0c2a
ndavis155e <59974240+ndavis155e@users.noreply.github.com>
2020-08-19 17:15:49 -0500

    Create hello.c

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Nickalus Davis
+ *
+ * Date: 08/17/2020
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Nickalus Davis\n");
+
+  return 0;
+}
